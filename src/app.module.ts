336aa4164c2be8d8ee29ee4ef71bfb6e091f0da6
Jerson Uyekita <jerson.uyekita@gok.digital>
2021-06-15 00:05:06 -0300

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
